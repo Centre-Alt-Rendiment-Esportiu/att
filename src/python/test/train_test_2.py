401f75e685ebf6d28de065883bd59319dfd43fef
Albert Sanso <albertsanso@gmail.com>
2016-02-16 20:38:06 +0100

    more

--- src/python/test/train_test_2.py
+++ src/python/test/train_test_2.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import sys
-sys.path.insert(0, '/home/asanso/workspace/att-spyder/att/src/python/')
+import platform
 
-import pygame
-
-import Queue
+PROJECT_ROOT = ""
+if platform.platform().lower().startswith("win"):
+	PROJECT_ROOT = "I:/dev/workspaces/python/att-workspace/att/"
+else:
+	if platform.platform().lower().startswith("linux"):
+		PROJECT_ROOT = "/home/asanso/workspace/att-spyder/att/"
+		
+sys.path.insert(0, PROJECT_ROOT + "/src/python/")
 
 from hit.serial.serial_port import *
 from hit.serial.serial_port_builder import *
@@ -23,7 +28,7 @@ def build_regressor_1():
 	processor = ATTMatrixHitProcessor()
 	regressor = ATTClassicHitRegressor(processor)
 
-	TRAIN_VALUES_FILE_LEFT = "/home/asanso/workspace/att-spyder/att/src/python/data/train_points_20160129_left.txt"	
+	TRAIN_VALUES_FILE_LEFT = "../data/train_points_20160129_left.txt"	
 	(hits_training_values, Y) = regressor.collect_train_hits_from_file(TRAIN_VALUES_FILE_LEFT)
 	print "Train Values: ", np.shape(hits_training_values), np.shape(Y)
 	
@@ -35,7 +40,7 @@ def build_regressor_2():
 	processor = ATTPlainHitProcessor()
 	regressor = ATTSkLearnHitRegressor(processor)
 	
-	TRAIN_VALUES_FILE_LEFT = "/home/asanso/workspace/att-spyder/att/src/python/data/train_points_20160129_left.txt"	
+	TRAIN_VALUES_FILE_LEFT = "../data/train_points_20160129_left.txt"	
 	(hits_training_values, Y) = regressor.collect_train_hits_from_file(TRAIN_VALUES_FILE_LEFT)
 	print "Train Values: ", np.shape(hits_training_values), np.shape(Y)
 	
--- src/python/view/view_test.py
+++ src/python/view/view_test.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import sys
-sys.path.insert(0, '/home/asanso/workspace/att-spyder/att/src/python/')
+import platform
+
+PROJECT_ROOT = ""
+if platform.platform().lower().startswith("win"):
+	PROJECT_ROOT = "I:/dev/workspaces/python/att-workspace/att/"
+else:
+	if platform.platform().lower().startswith("linux"):
+		PROJECT_ROOT = "/home/asanso/workspace/att-spyder/att/"
+		
+sys.path.insert(0, PROJECT_ROOT + "/src/python/")
 
 import pygame
+from pygame.locals import *
 
 import Queue
 
@@ -18,17 +28,21 @@ from hit.train.regressor import ATTSkLearnHitRegressor
 
 import numpy as np
 
-windowWidth = 1096 
-windowHeight = 608
+windowWidth = 1024 
+windowHeight = 768
 tableLineWidth = 4
 
 SCREEN_MULITPLIER = 14.6
+
+
+TRAIN_DATA_FILE = "../data/train_points_20160129_left.txt"
+HITS_DATA_FILE = "../../arduino/data/train_20160129_left.txt"
 
 def build_regressor():
 	processor = ATTMatrixHitProcessor()
 	regressor = ATTClassicHitRegressor(processor)
 	
-	(hits_training_values, Y) = regressor.collect_train_hits_from_file("/home/asanso/workspace/python/att/src/python/test/data/train_points_2.txt")
+	(hits_training_values, Y) = regressor.collect_train_hits_from_file(TRAIN_DATA_FILE)
 	print "Train Values: ", np.shape(hits_training_values), np.shape(Y)
 	
 	regressor.train(hits_training_values, Y)
@@ -56,7 +70,30 @@ def displayTable(windowWidth, windowHeight, tableLineWidth, ball):
     pygame.draw.circle(window, ball.color, ball.position, ball.radius, ball.radius)
     
     pygame.display.update()
-				
+
+
+def toggle_fullscreen():
+    screen = pygame.display.get_surface()
+    tmp = screen.convert()
+    caption = pygame.display.get_caption()
+    cursor = pygame.mouse.get_cursor()  # Duoas 16-04-2007 
+    
+    w,h = screen.get_width(),screen.get_height()
+    flags = screen.get_flags()
+    bits = screen.get_bitsize()
+    
+    pygame.display.quit()
+    pygame.display.init()
+    
+    screen = pygame.display.set_mode((w,h),flags^FULLSCREEN,bits)
+    screen.blit(tmp,(0,0))
+    pygame.display.set_caption(*caption)
+ 
+    pygame.key.set_mods(0) #HACK: work-a-round for a SDL bug??
+ 
+    pygame.mouse.set_cursor( *cursor )  # Duoas 16-04-2007
+    
+    return screen
 
 if __name__ == '__main__':
 	
@@ -70,14 +107,15 @@ if __name__ == '__main__':
 		
 	#builder = DummySerialPortBuilder()
 	#builder = ATTEmulatedSerialPortBuilder()
-	#builder = ATTHitsFromFilePort2Builder()
-	builder = ATTArduinoSerialPortBuilder()
+	builder = ATTHitsFromFilePortBuilder()
+	#builder = ATTArduinoSerialPortBuilder()
 	
 	processor = ATTMatrixHitProcessor()
 	regressor = ATTClassicHitRegressor(processor)
-	#serial_reader = ATTHitsFromFilePort2()
-	#serial_reader = ATTHitsFromFilePort_TrainPoints()
-	serial_reader = ATTArduinoSerialPort(port, baud)
+	
+	port = HITS_DATA_FILE
+	serial_reader = ATTHitsFromFilePort(port, baud)
+	#serial_reader = ATTArduinoSerialPort(port, baud)
 	
 	
  	pygame.init()
@@ -89,23 +127,36 @@ if __name__ == '__main__':
 	ball = Ball()
 	
 	screen = pygame.display.set_mode((int(round(2 * 54 * SCREEN_MULITPLIER)), int(round(60 * SCREEN_MULITPLIER))))
+	
+	#toggle_fullscreen()
+	
 	clock = pygame.time.Clock()
 
 	done = False
 	
-	screen.fill((250, 250, 250))
+	#screen.fill((250, 250, 250))
 	displayTable(windowWidth, windowHeight, tableLineWidth, ball)
 												
 	while not done:
+		DELAY = 300
 		
 		if not workQueue.empty():
 			reading = workQueue.get()
-			print "> "+reading
-			(y,x) = regressor.predict(reading) # (6,6)
-			ball.position = (int(x*7.5),int(y*8))
-			print (x,y)
-			pygame.draw.circle(window, ball.color, ball.position, ball.radius, ball.radius)
-			pygame.display.update()
+			if reading <> "":
+				print "> "+reading
+				DELAY = 50
+				
+				
+				try:
+					(y,x) = regressor.predict(reading) # (6,6)
+					#ball.position = (int(x*7.5),int(y*8))
+					ball.position = (int(x*11),int(y*10))
+					print (x,y)
+					pygame.draw.circle(window, ball.color, ball.position, ball.radius, ball.radius)
+				except:
+					pass
+				
+				pygame.display.update()
 		
 			
 		for event in pygame.event.get():
@@ -119,10 +170,11 @@ if __name__ == '__main__':
 					done = True
 					
 												
-			"""
 			if event.type == pygame.QUIT:
 				done = True
-			"""
+		
+		pygame.time.delay(DELAY)
+
 		"""	
 		pressed = pygame.key.get_pressed()
 		if not(pressed[pygame.K_UP]) and not(pressed[pygame.K_DOWN]) and not(pressed[pygame.K_RETURN]) and not(pressed[pygame.K_ESCAPE]):
