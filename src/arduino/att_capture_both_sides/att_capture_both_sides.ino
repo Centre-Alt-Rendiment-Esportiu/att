957f6b880a7e9e6e8e40fd87a3a3188f43a96811
Albert Sanso <albertsanso@gmail.com>
2016-03-01 00:36:45 +0100

    .

--- src/arduino/att_capture_both_sides/att_capture_both_sides.ino
+++ src/arduino/att_capture_both_sides/att_capture_both_sides.ino
@@ -6,16 +6,8 @@
  * Augmented Table Tennis
  * -------------
  * By Andrea Amodeo and Erik Einebrant
- *
- * Continued by:
- *
- * Jordi Serres Marimon
- * Eloi Puertas Prats
- * Oscar Baño Perez
- * Albert Sans Garrido
- *
  */
- 
+
 // Constants
 const int THRESHOLD = 4;
 const int SENSOR_COUNT = 8;
@@ -24,7 +16,7 @@ const int SENSOR_COUNT = 8;
 struct Hit {
   unsigned long timings[SENSOR_COUNT];
   int volumes[SENSOR_COUNT];
-  
+
   unsigned long startTime;
   char side;
   boolean reported;  // have we sent it off via serial yet?
@@ -33,66 +25,80 @@ struct Hit {
 //Pin mapping of Teensy
 int rightPins[] = { A8, A9, A10, A11, A12, A13, A14, A15}; // clockwise from top left
 int leftPins[] = { A0, A1, A2, A3, A4, A5, A6, A7}; // clockwise from top left
+int rightThresholds[] = {10, 4, 4, 4, 4, 4, 4, 10};
+int leftThresholds[] = {4, 4, 4, 4, 4, 4, 4, 10};
 
 const unsigned long NO_RECORD = -1;
-const unsigned long TIMEOUT_BETWEEN_HITS = 300000; // in millis
+const unsigned long TIMEOUT_BETWEEN_HITS = 10000; // in micros
 const char LEFT = 'l';
 const char RIGHT = 'r';
+const char UNDEF = 'u';
+Hit curHit;
 
-Hit curHit = {NO_RECORD, NO_RECORD, NO_RECORD, NO_RECORD, NO_RECORD, NO_RECORD, NO_RECORD, NO_RECORD, LEFT, false};
 
 void setup() {
+  createNewHit();
   Serial.begin(115200);
 }
 
 void loop() {
   //delay(5);
-  readPins(leftPins, LEFT);
-  readPins(rightPins, RIGHT);
-  
+  if (curHit.side == UNDEF || curHit.side == LEFT) {
+      readPins(leftPins, leftThresholds, LEFT);
+    }
+  if (curHit.side == UNDEF || curHit.side == RIGHT){
+      readPins(rightPins, rightThresholds, RIGHT);
+    }
+}
+
   //Serial.println(curHit.volumes[0]);
-  
+
   if (isCompleteHit(&curHit) && !curHit.reported) {
     // print the current hit status
     Serial.print("hit: {");
-    for(int i = 0; i < SENSOR_COUNT; i++){
-      //Serial.print(curHit.timings[i]);
-      //Serial.print(":");
+    for (int i = 0; i < SENSOR_COUNT; i++) {
+      Serial.print(curHit.timings[i]);
+      Serial.print(":");
       Serial.print(curHit.volumes[i]);
       Serial.print(" ");
     }
     Serial.print(curHit.side);
     Serial.println("}");
-    
+
     curHit.reported = true;
   }
-  
-  if(hitTimedOut()) {
-    // create a new hit
-    Hit newHit;
-    for(int i = 0; i < SENSOR_COUNT; i++){
-      newHit.timings[i] = NO_RECORD;
-      newHit.volumes[i] = NO_RECORD;
-    }
-    
-    newHit.side = LEFT;
-    newHit.startTime = NO_RECORD;
-    newHit.reported = false;
-    
-    curHit = newHit;
+
+  if (hitTimedOut()) {
+    createNewHit();
   }
+}
+
+
+void createNewHit() {
+  // create a new hit
+  Hit newHit;
+  for (int i = 0; i < SENSOR_COUNT; i++) {
+    newHit.timings[i] = NO_RECORD;
+    newHit.volumes[i] = NO_RECORD;
+  }
+
+  newHit.side = UNDEF;
+  newHit.startTime = NO_RECORD;
+  newHit.reported = false;
+
+  curHit = newHit;
 }
 
 /*
  * For the list of one sides pins, see if any of them is detecting an analog reading above the threshold
  * and if so, report a hit with side and detection time
  */
-void readPins(int* pinList, char curSide) {
+void readPins(int* pinList, int* thresholds, char curSide) {
   for (int i = 0; i < SENSOR_COUNT; i++) {
     int pinNumber = pinList[i];
     int pinValue = analogRead(pinNumber);
     //Serial.println(pinValue);
-    if (pinValue >= THRESHOLD) {
+    if (pinValue >= thresholds[i]) {
       unsigned long detectionTime = micros();
       if (curHit.startTime == NO_RECORD) {
         curHit.startTime = detectionTime;
@@ -100,16 +106,16 @@ void readPins(int* pinList, char curSide) {
       }
       // add this data to the existing hit
       updateCorner(pinNumber, curSide, detectionTime, pinValue, pinList);
-     }
+    }
   }
 }
 
 /*
  * Updates the current hit data structure in the correct corner, given the pin number
  */
-void updateCorner(int pinNumber, char curSide, unsigned long detectionTime, int pinValue, int* sidePins){
-  for(int i = 0; i < SENSOR_COUNT; i++){
-    if(pinNumber == sidePins[i] && curHit.timings[i] == NO_RECORD){
+void updateCorner(int pinNumber, char curSide, unsigned long detectionTime, int pinValue, int* sidePins) {
+  for (int i = 0; i < SENSOR_COUNT; i++) {
+    if (pinNumber == sidePins[i] && curHit.timings[i] == NO_RECORD) {
       curHit.timings[i] = detectionTime - curHit.startTime;
       curHit.volumes[i] = pinValue;
     }
@@ -124,7 +130,7 @@ boolean hitTimedOut() {
 
 boolean isCompleteHit(struct Hit *hit) {
   boolean isHit = true;
-  for(int i = 0; i < SENSOR_COUNT; i++){
+  for (int i = 0; i < SENSOR_COUNT; i++) {
     isHit = isHit && hit->timings[i] != NO_RECORD;
   }
   return isHit;
