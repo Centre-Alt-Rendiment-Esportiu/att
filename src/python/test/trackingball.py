e523ab52481bb58c0633963f81caf2ee3fa9ab2e
Eloi Puertas <eloi@MacBook-Air-de-Eloi.local>
2016-04-10 10:07:34 +0200

    tracking is ok. Fucking A

--- src/python/test/trackingball.py
+++ src/python/test/trackingball.py
@@ -47,14 +47,15 @@ camera = cv2.VideoCapture(VIDEODEV); assert camera.isOpened()
 colorLower = (251,251,251)
 colorUpper = (255,255,255)
 
-tailsize = 10
+tailsize = 16
 pts = deque(maxlen=tailsize)
 color = [(0, 0, 255),(0, 255, 255)]
 colorIndx = 0
 
 while True:
     # grab the current frame
-    (grabbed, frame) = camera.read()
+    (grabbed, frameWhole) = camera.read()
+
     if  not grabbed :
             camera.set(cv2.CAP_PROP_FRAME_COUNT, 0)
             camera.release()
@@ -64,45 +65,29 @@ while True:
             time.sleep(0.02)
             continue
      # show the frame to our screen
-    #frame = resize(frame, width=600)
-    #blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    #hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-     # show the frame to our screen
-    frame = frame[130:385, 82:560]
+    frame = frameWhole[133:380, 85:555]
     mask = cv2.inRange(frame, colorLower, colorUpper)
-   # mask = cv2.erode(mask, None, iterations=2)
-   # mask = cv2.dilate(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
     center = None
     # only proceed if at least one contour was found
+    reset  = True
     if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
         cmax = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(cmax)
         M = cv2.moments(cmax)
-        if center is not None :
-             cv2.circle(center, (int(x), int(y)), 5,(0, 255, 0), -1)
         if M["m00"] > 0 :
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        # only proceed if the radius meets a minimum size
-        #if radius > 0:
-            # draw the minimum enclosing circle, yellow colour in BGR format(), thickness = 2
             cv2.circle(frame,center, 5,(0, 255, 0), -1)
-            # draw the centroid,radius 5, red colour in BGR format(), thikness < 0 meaning that the circle is filled.
-
-            #cv2.putText(frame, "Centroid  x: {}, y: {}".format(center[0], center[1]),(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-             #           0.35, (0, 0, 255), 1)
-            #cv2.putText(frame, "minEnclosing: x: {}, y: {}".format(int(x), int(y)),(10, frame.shape[0] - 30), cv2.FONT_HERSHEY_SIMPLEX,
-            #            0.35, (0, 0, 255), 1)
-            ## IF Change of sense
-            print center
-            if (len(pts)> 1) and ((pts[0][0] - center[0])< 0) :
-                pts.clear()
-                colorIndx = (colorIndx+1) % 2
+            # calculate direction of ball, paint it ,if change clear
+            if (len(pts)> 0) and ((pts[0][0] - center[0])< 0) :
+                if colorIndx != 0 :
+                    colorIndx = 0
+                    pts.clear()
+            elif (len(pts)> 0) and ((pts[0][0] - center[0]) > 0) :
+                 if colorIndx != 1 :
+                    colorIndx = 1
+                    pts.clear()
             pts.appendleft(center)
-
 
     # loop over the set of tracked points
     for i in xrange(1, len(pts)):
@@ -110,17 +95,14 @@ while True:
         # them
         if pts[i - 1] is None or pts[i] is None:
             continue
-
-        # otherwise, compute the thickness of the line and
-        # draw the connecting lines
-      #  cv2.circle(frame, pts[i], 5, (0, 0, 255), -1)
         thickness = 3
         cv2.line(frame,pts[i - 1], pts[i],color[colorIndx] , thickness)
 
     # show the frame to our screen
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
-
+    cv2.imshow("AllFrame", frameWhole)
+    time.sleep(0.5)
     if cv2.waitKey(1) & 0xFF is ord('q'):
         break
 
